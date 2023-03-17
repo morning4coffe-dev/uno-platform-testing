c4c8f63b9b73bb1c77d63f23349c8280d6bc3292
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2023-03-17 19:35:31 +0100

    Grid View Binding Issue

--- UnoPlatformTesting/UnoPlatformTesting/UnoPlatformTesting/MainPage.xaml.cs
+++ UnoPlatformTesting/UnoPlatformTesting/UnoPlatformTesting/MainPage.xaml.cs
@@ -7,8 +7,10 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -21,6 +23,39 @@ namespace UnoPlatformTesting
         public MainPage()
         {
             this.InitializeComponent();
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            ButtonContent = "Hello";
+        }
+
+
+        public string ButtonContent
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Dictionary<string, object> _propertyValueStore = new Dictionary<string, object>();
+
+        protected T GetProperty<T>([CallerMemberName] string propertyName = null)
+        {
+            return _propertyValueStore.TryGetValue(propertyName, out var value) ? (T)value : default;
+        }
+
+        protected void SetProperty<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!(_propertyValueStore.TryGetValue(propertyName, out var oldValue) && oldValue?.Equals(value) == true))
+            {
+                _propertyValueStore[propertyName] = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
