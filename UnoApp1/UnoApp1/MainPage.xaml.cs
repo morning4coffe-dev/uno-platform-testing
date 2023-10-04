0dfff6e42ca56b290bae2c603c6ce03a2d4ad261
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2023-10-04 22:32:30 +0200

    chore: AutosuggestBox outline issue

--- UnoApp1/UnoApp1/MainPage.xaml.cs
+++ UnoApp1/UnoApp1/MainPage.xaml.cs
@@ -1,25 +1,9 @@
-using System.Collections.ObjectModel;
-
 namespace UnoApp1;
 
 public sealed partial class MainPage : Page
 {
-    public ObservableCollection<MyData> Items { get; set; } = new();
-    public MyData SelectedItem { get; set; }
-
     public MainPage()
     {
         this.InitializeComponent();
-
-        Items.Add(new MyData { Name = "Item 1", Description = "Description 1" });
-        Items.Add(new MyData { Name = "Item 2", Description = "Description 2" });
-        Items.Add(new MyData { Name = "Item 3", Description = "Description 3" });
-        Items.Add(new MyData { Name = "Item 4", Description = "Description 4" });
-    }
-
-    public class MyData
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
     }
 }
